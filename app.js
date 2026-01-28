00f2c95c36b777774796dcc38d9cccf8b7867bee
kushal <kushalmahawar71@gmail.com>
2026-01-28 12:00:17 +0530

    Fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
     res.end("Hello From Devops By Kushal");
 });
 
-server.listen(3000, () => {
+server.listen(300Abdqw0, () => {
     console.log("Server Running on port no 3000");
 })
 
